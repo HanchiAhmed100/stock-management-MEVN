5730d9586e409fd6acc1cb3c591bdd28085b6b09
HanchiAhmed100 <36990709+HanchiAhmed100@users.noreply.github.com>
2018-10-29 13:45:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ let app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static("public"))
+app.use(express.static(__dirname+"/public"))
 
 //Router initalisation
 let Articles = require('./routes/api/Articles')
